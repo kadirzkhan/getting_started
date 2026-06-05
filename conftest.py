9108ca94171e3f0245a1604ef205fc308f42e1e8
kadirzkhan <abdulkadir5108@gmail.com>
2026-06-05 12:36:13 +0530

    Done

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from playwright.sync_api import Page
+from pages.login_page import LoginPage
+
+@pytest.fixture
+def logged_in_page(page: Page):
+    login_page = LoginPage(page)
+    login_page.open()
+    
+    login_page.login("standard_user", "secret_sauce")
+    return page
